c218d7c2293396c7bf48b6a74d346ce373d25f15
PAVAN SHIRSATH <158544111+flywitpavan@users.noreply.github.com>
2024-10-19 18:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+describe('Audio Section Test', () => {
+    it('should contain the heading "3 Random Audios"', () => {
+        // Visit the page
+        cy.visit(baseUrl + "/main.html");
+
+        // Increase timeout and check for the heading with correct case sensitivity
+        cy.contains('3 Random Audios', { timeout: 10000 }).should('exist');
+    });
+});
